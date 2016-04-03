f629bc18d79d2fc7a1f0818673a16de237fcd7a3
Jonah Möller <jonah.moeller@the-white-label.com>
2016-04-03 20:57:06 +0200

    added a welcome message for the api

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.use('/api/users', require('./users/router.js'));
 app.use('/api/categories', require('./categories/router.js'));
 
 // Default Route
+app.use('/', function(req, res){
+	res.send('welcome to the waiter api! check out all the options at the endpoint /resources/. the complete repository can be found on github. https://github.com/waiterio/api/');
+});
+
+// Default Fallback Route
 app.use(function(req, res) {
 	res.status(404).end();
 });
