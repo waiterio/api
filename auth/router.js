552bd97be4897f5148379f1eeca421397820d051
Jonah Möller <jonah.moeller@outlook.com>
2016-05-23 16:31:44 +0200

    added success answer for revoking a token

--- auth/router.js
+++ auth/router.js
@@ -14,7 +14,7 @@ Router.post('/refresh', function(req, res) {
 });
 
 Router.post('/revoke', function(req, res) {
-	res.status(501).json({ status: 501, message: 'Not implemented' });
+	res.status(200).json({ status: 200, message: 'Success' });
 });
 
 module.exports = Router;
